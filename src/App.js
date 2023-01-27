11390b90dcb62f591f12f63206d28c7619872924
Jerin jj <jerinjj202@gmail.com>
2023-01-27 23:23:15 +0530

    Product section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import Header from './Components/Header/Header';
+import Products from './Components/Products/Products';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Banner/>
+      <Products/>
     </div>
   );
 }
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Products.css'
+
+const Products = () => {
+  return (
+    <div className='Products'>
+      
+    </div>
+  )
+}
+
+export default Products
